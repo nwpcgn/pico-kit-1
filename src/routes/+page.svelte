c55df158f9fdd8f5fedd13b92dd346fa8fa83b14
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2024-11-30 20:13:42 +0100

    init app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+
+	let { data } = $props();
+</script>
+
+<h1 class="text-3xl font-bold text-[#ff3e00]">
+	Hello {data.name}!
+</h1>
+
+<Counter />
