a24fec28a273d0ff90abdd23ea5da076db46ffc3
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2024-12-01 23:03:46 +0100

    ua

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from '../../../../src/routes/sandbox/+page.ts'
-export { universal }
-export { default as component } from '../../../../src/routes/sandbox/+page.svelte'
--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from '../../../../src/routes/ui/+page.ts'
-export { universal }
-export { default as component } from '../../../../src/routes/ui/+page.svelte'
